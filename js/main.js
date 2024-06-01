cab25bd75fd6c85a1a16c47e166fc8d57d6bc073
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-06-01 23:07:22 +0530

    2. sessionStorage and localStorage

--- js/main.js
+++ js/main.js
@@ -4,13 +4,22 @@
 // available to javascript via global variable = window;
 // we don't have to type window it is implied:
 
-window.alert("ok lol");
-alert("ok lol!");
+const myObject = {
+    name: "jack",
+    logName: function() {
+        console.log(this.name);
+    }
+}
 
+const myArray = ["eat", "code", "sleep"];
+// sessionStorage ==>> setItem(key: string, value: string) requires a name for the storage or to store something in it 
+window.sessionStorage.setItem("mySessionStorage", myObject);
+const mySessionData = sessionStorage.getItem("mySessionStorage");
+console.log(mySessionData);
 
-
-
-
+window.sessionStorage.setItem("mySessionStorage2", myArray);
+const mySessionData2 = sessionStorage.getItem("mySessionStorage2");
+console.log(mySessionData2);
 
 
 
