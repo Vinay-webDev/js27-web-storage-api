eb750299891b47462523828f7ebafb2feea67fe2
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-06-01 22:42:38 +0530

    1. defination

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// web storage API 
+// web storage API is not a part of virtual DOM
+// refers to window API
+// available to javascript via global variable = window;
+// we don't have to type window it is implied:
+
+window.alert("ok lol");
+alert("ok lol!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
