f5aa5ca1b5664345f9c10ef977a1c8ad3c44ae61
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-06-02 13:10:43 +0530

    6. difference between localStorage and sessionStorage

--- js/main.js
+++ js/main.js
@@ -33,6 +33,7 @@ const myObject = {
 sessionStorage.setItem("mySessionStore", JSON.stringify(myObject));
 const sessionData = JSON.parse(sessionStorage.getItem("mySessionStore"));
 console.log(sessionData); */
+/*
 const myArray1 = ["eat", "fight", "sleep"];
 
 sessionStorage.setItem("mySessionStore1", myArray1);
@@ -43,10 +44,24 @@ const myArray = ["eat", "fight", "sleep"];
 
 sessionStorage.setItem("mySessionStore", JSON.stringify(myArray));
 const sessionData = JSON.parse(sessionStorage.getItem("mySessionStore"));
-console.log(sessionData);
+console.log(sessionData); */
+/////////////////////////////////////////////////////////////////////
+const myArray = ["eat", "code", "sleep"];
+const myObject = {
+    name: "jack",
+    hobbies: ["eat", "code", "sleep"],
+    logName: function () {
+        console.log(this.name);
+    }
+}
+// localStorage = is a persistant storage means whenever we close the web app the data is still available when we open the web app again
+// sessionStorage = is a temporary storage which will be gone if close out the web app 
+localStorage.setItem("myLocalStore", JSON.stringify(myObject));
 
+const localData = JSON.parse(localStorage.getItem("myLocalStore"));
 
-
+console.log(localData);
+console.log( typeof localData);
 
 
 
